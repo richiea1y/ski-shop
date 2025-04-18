991c8f682df9febc2cf171b7761bc8a0aed6f5dd
richiea1y <44350561+richiea1y@users.noreply.github.com>
2025-04-18 16:54:19 +0800

    chore: set up config of ESLint, Prettier and setting.json
    - copy past ESLint template of fortes
    - copy past Prettier template of fortes
    - copy past setting.json template of fortes

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,60 @@
-import { defineConfig, globalIgnores } from 'eslint/config'
-import globals from 'globals'
 import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
 import pluginVitest from '@vitest/eslint-plugin'
 import pluginPlaywright from 'eslint-plugin-playwright'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
-export default defineConfig([
+export default [
   {
     name: 'app/files-to-lint',
     files: ['**/*.{js,mjs,jsx,vue}'],
   },
 
-  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
-
   {
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-      },
-    },
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
   },
 
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
-  
+
+  {
+    name: 'app/custom-rules',
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', {  // 使用 TypeScript 的檢查替代
+        'argsIgnorePattern': '^_',
+        'varsIgnorePattern': '^_',
+        'caughtErrorsIgnorePattern': '^_',
+        'ignoreRestSiblings': true,
+      }],
+      'vue/no-unused-components': ['warn', {
+        ignoreWhenBindingPresent: true
+      }],
+      'vue/multi-word-component-names': 'off',
+      'vue/no-v-html': 'off',
+      'no-undef': 'error',
+      'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+      'vue/html-self-closing': ['error', {
+        html: {
+          void: 'always',
+          normal: 'always',
+          component: 'always'
+        },
+        svg: 'always',
+        math: 'always'
+      }],
+    },
+  },
+
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
   },
-  
+
   {
-    ...pluginPlaywright.configs['flat/recommended'],
+    ...pluginPlaywright.configs.recommended,
     files: ['e2e/**/*.{test,spec}.{js,ts,jsx,tsx}'],
   },
   skipFormatting,
-])
+]
