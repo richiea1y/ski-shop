dc152f5e7eee0c502043fd0376e1b178de65bcad
richiea1y <44350561+richiea1y@users.noreply.github.com>
2025-04-18 18:12:03 +0800

    chore: clean up project and adjust App and Main.js
    - Add some scss styles template from fortes
    - copy paste main.js template from fortes
    - copy paste App.vue template from fortes
    - clean up unnecessary vue files, css files and img

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
-import './assets/main.css'
+import '@/styles/tw.css'
+import '@/styles/style.scss'
+import 'element-plus/dist/index.css'
+import 'virtual:svg-icons-register'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+// import SvgIcon from '@/components/SvgIcon.vue';
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +15,11 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+
+// 全局註冊 SvgIcon 組件，給自定義 icon 使用
+// app.component('SvgIcon', SvgIcon);
 
 app.mount('#app')
